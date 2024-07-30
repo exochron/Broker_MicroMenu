80e301d417d93ae27005b60922c5c5efba506aa2
eXochron <exochron@freenet.de>
2024-07-30 22:46:34 +0200

    updated for 11.0 using new menu instead of LibQTip

--- Broker_MicroMenu.lua
+++ Broker_MicroMenu.lua
@@ -1,12 +1,10 @@
 -- Broker_MicroMenu by yess
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1",true)
-local LibQTip = LibStub('LibQTip-1.0')
 local L = LibStub("AceLocale-3.0"):GetLocale("Broker_MicroMenu")
 
 local _G, floor, string, GetNetStats, GetFramerate  = _G, floor, string, GetNetStats, GetFramerate
 local delay, counter = 1,0
-local dataobj, tooltip, db
-local color = true
+local dataobj, db
 local _
 local addonName = Broker_MicroMenuEmbeddedName or "Broker_MicroMenu"
 local path = Broker_MicroMenuEmbeddedPath or "Interface\\AddOns\\Broker_MicroMenu\\media\\"
@@ -44,42 +42,8 @@ dataobj = ldb:NewDataObject(addonName, {
 		else
 			_G.ToggleCharacter("PaperDollFrame")
 		end
-		LibQTip:Release(tooltip)
-		tooltip = nil
 	end
 })
-
--------------------------
--- custom libqtip cell
--------------------------
-local myProvider, cellPrototype = LibQTip:CreateCellProvider()
-
-function cellPrototype:InitializeCell()
-	self.texture = self:CreateTexture()
-	self.texture:SetAllPoints(self)
-end
-
-function cellPrototype:SetupCell(tooltip, value, justification, font, iconCoords, unitID,guild)
-	local tex = self.texture
-	tex:SetWidth(16)
-	tex:SetHeight(16)
-
-	if guild then
-		_G.SetSmallGuildTabardTextures("player", tex,tex);
-	elseif unitID then
-		_G.SetPortraitTexture(tex, unitID)
-	else
-		tex:SetTexture(value)
-	end
-	if iconCoords then
-		tex:SetTexCoord(_G.unpack(iconCoords))
-	end
-	return tex:GetWidth(), tex:GetHeight()
-end
-
-function cellPrototype:ReleaseCell()
-
-end
 
 -------------------------
 
@@ -142,167 +106,111 @@ function dataobj:UpdateText()
 	end
 end
 
-local function MouseHandler(event, func, button, ...)
-	local name = func
-
-	if _G.type(func) == "function" then
-		func(event, func,button, ...)
-	else
-		func:GetScript("OnClick")(func,button, ...)
-	end
-
-	LibQTip:Release(tooltip)
-	tooltip = nil
-end
+local menu
 
 function dataobj:OnEnter()
-	if tooltip then
-		LibQTip:Release(tooltip)
-	end
 
-	tooltip = LibQTip:Acquire(addonName.."Tooltip", 2, "LEFT", "LEFT")
-	tooltip:Clear()
-	self.tooltip = tooltip
+	local generator = function(owner, root)
+		root:SetTag(addonName);
 
-	local y, x = tooltip:AddLine()
-	tooltip:SetCell(y, 1, "", myProvider, {0.2, 0.8, 0.2, 0.8},"player")
-	local ckey = _G.GetBindingKey("TOGGLECHARACTER0")
-	if ckey then
-		tooltip:SetCell(y, 2, _G.CHARACTER_BUTTON.."|cffffd200 ("..ckey..")")
-	else
-		tooltip:SetCell(y, 2, _G.CHARACTER_BUTTON)
-	end
-	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function() _G.ToggleCharacter("PaperDollFrame") end)
+		local microbuttons = { -- or see MICRO_BUTTONS
+			"CharacterMicroButton",
+			"ProfessionMicroButton",
+			"PlayerSpellsMicroButton",
+			"AchievementMicroButton",
+			"QuestLogMicroButton",
+			"GuildMicroButton",
+			"LFDMicroButton",
+			"EJMicroButton",
+			"CollectionsMicroButton",
+			"StoreMicroButton",
+			"MainMenuMicroButton",
+		}
 
-	local y, x = tooltip:AddLine()
-	tooltip:SetCell(y, 1, path.."spells.tga", myProvider)
-	local key = _G.GetBindingKey("TOGGLESPELLBOOK")
-	if key then
-		tooltip:SetCell(y, 2, _G.SPELLBOOK_ABILITIES_BUTTON.."|cffffd200 ("..key..")")
-	else
-		tooltip:SetCell(y, 2, _G.SPELLBOOK_ABILITIES_BUTTON)
-	end
-	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function(self, func, button, ...)
+		for _, microButtonName in ipairs(microbuttons) do
+			local microButton = _G[microButtonName]
+			if microButton and microButton:IsEnabled() then
+				local clickhandler = function()
+					microButton:GetScript("OnClick")(microButton)
+				end
+				if microButtonName == "MainMenuMicroButton" then
+					clickhandler = mainmenu
+				end
 
-		if _G.InCombatLockdown() then
-			if key then
-				_G.DEFAULT_CHAT_FRAME:AddMessage("Cant' open the Spellbook during combat. Use your hot key: "..key)
-			else
-				_G.DEFAULT_CHAT_FRAME:AddMessage("Cant' open the Spellbook during combat. Set and use a hot key.")
+				local button = root:CreateButton(microButton.tooltipText, clickhandler)
+				button:AddInitializer(function(button)
+					local icon = button:AttachTexture()
+					icon:SetSize(16, 20)
+					icon:SetPoint("LEFT", button, "LEFT")
+
+					if microButton.Portrait then
+						_G.SetPortraitTexture(icon, "player")
+					else
+						icon:SetAtlas("UI-HUD-MicroMenu-"..microButton.textureName.."-Up")
+						icon:SetVertexColor(microButton:GetNormalTexture():GetVertexColor())
+					end
+
+					if microButton.Emblem then
+						local emblem = button:AttachTexture()
+						emblem:SetSize(6, 7)
+						emblem:SetPoint("CENTER", icon, "CENTER", 0, 1)
+						emblem:SetDrawLayer("OVERLAY")
+						_G.SetSmallGuildTabardTextures("player", emblem, emblem)
+					end
+
+					button.fontString:SetPoint("LEFT", icon, "RIGHT")
+				end)
 			end
-		else
-			_G.ToggleSpellBook(_G.BOOKTYPE_SPELL)
+		end
+
+		root:QueueSpacer()
+
+		local function buildCenterButton(text, func)
+			local element = root:CreateButton(text, func)
+			element:AddInitializer(function(button)
+				button.fontString:ClearAllPoints()
+				button.fontString:SetPoint("CENTER")
+			end)
+		end
+		buildCenterButton(GAMEMENU_OPTIONS, function()
+			SettingsPanel:Open()
+		end)
+		buildCenterButton(ADDONS, function()
+			ShowUIPanel(AddonList)
+		end)
+		if C_SplashScreen.CanViewSplashScreen() then
+			buildCenterButton(GAMEMENU_NEW_BUTTON, function()
+				C_SplashScreen.RequestLatestSplashScreen(true)
+			end)
+		end
+		buildCenterButton(HUD_EDIT_MODE_MENU, function()
+			ShowUIPanel(EditModeManagerFrame)
+		end)
+		buildCenterButton(MACROS, function()
+			ShowMacroFrame()
+		end)
+	end
+
+	local elementDescription = MenuUtil.CreateRootMenuDescription(MenuVariants.GetDefaultContextMenuMixin())
+
+	Menu.PopulateDescription(generator, self, elementDescription)
+
+	local anchor = CreateAnchor("TOPLEFT", self, "BOTTOMLEFT", 0, 5)
+	menu = Menu.GetManager():OpenMenu(self, elementDescription, anchor)
+	menu:SetClosedCallback(function()
+		menu = nil
+	end)
+	menu:HookScript("OnLeave", function()
+		if not menu:IsMouseOver() then
+			menu:Close()
 		end
 	end)
+end
 
-	local y, x = tooltip:AddLine()
-	tooltip:SetCell(y, 1, path.."talents.tga", myProvider)
-	tooltip:SetCell(y, 2, _G.TalentMicroButton.tooltipText)
-	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function(self, func, button, ...)
-		if _G.InCombatLockdown() then
-			key = _G.GetBindingKey("TOGGLETALENTS")
-			if key then
-				_G.DEFAULT_CHAT_FRAME:AddMessage("Cant' open the Talents during combat. Use your hot key: "..key)
-			else
-				_G.DEFAULT_CHAT_FRAME:AddMessage("Cant' open the Talents during combat. Set and use a hot key.")
-			end
-		else
-			_G.LoadAddOn("Blizzard_TalentUI")
-			if  _G.PlayerTalentFrame:IsShown() then
-				_G.PlayerTalentFrame:Hide()
-			else
-				_G.tinsert(_G.UISpecialFrames,_G.PlayerTalentFrame:GetName());
-				_G.PlayerTalentFrame:Show()
-			end
-		end
-	end)
-
-	local y, x = tooltip:AddLine()
-	if AchievementMicroButton then
-		tooltip:SetCell(y, 1, path.."achivements.tga", myProvider)
-		tooltip:SetCell(y, 2, _G.AchievementMicroButton.tooltipText)
-		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.AchievementMicroButton)
+function dataobj:OnLeave(...)
+	if menu and not menu:IsMouseOver() then
+		menu:Close()
 	end
-
-	if QuestLogMicroButton then
-		local y, x = tooltip:AddLine()
-		tooltip:SetCell(y, 1, path.."quest.tga", myProvider)
-		tooltip:SetCell(y, 2, _G.QuestLogMicroButton.tooltipText)
-		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.QuestLogMicroButton)
-	end
-
-	if _G.GuildMicroButton then
-		local y, x = tooltip:AddLine()
-		tooltip:SetCell(y, 1, "", myProvider, nil,"player",true)
-		tooltip:SetCell(y, 2, _G.GuildMicroButton.tooltipText)
-		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.GuildMicroButton)
-	end
-
-	if _G.LFDMicroButton then
-		local y, x = tooltip:AddLine()
-		tooltip:SetCell(y, 1, path.."lfg.tga", myProvider)
-		tooltip:SetCell(y, 2, _G.LFDMicroButton.tooltipText)
-		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.LFDMicroButton)
-	end
-
-	if _G.CollectionsMicroButton then
-		local y, x = tooltip:AddLine()
-		tooltip:SetCell(y, 1, path.."mounts.tga", myProvider)
-		local clkey = _G.GetBindingKey("TOGGLECOLLECTIONS")
-		if clkey then
-			tooltip:SetCell(y, 2, _G.COLLECTIONS.."|cffffd200 ("..clkey..")")
-		else
-			tooltip:SetCell(y, 2, _G.COLLECTIONS)
-		end
-		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.CollectionsMicroButton)
-	end
-
-	if _G.EJMicroButton then
-		local y, x = tooltip:AddLine()
-		tooltip:SetCell(y, 1, path.."journal.tga", myProvider)
-		tooltip:SetCell(y, 2, _G.EJMicroButton.tooltipText)
-		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.EJMicroButton)
-	end
-	if _G.RaidMicroButton then
-		local y, x = tooltip:AddLine()
-		tooltip:SetCell(y, 1, path.."raid.tga", myProvider)
-		tooltip:SetCell(y, 2, _G.RaidMicroButton.tooltipText)
-		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.RaidMicroButton)
-	end
-
-	if _G.StoreMicroButton then
-		local y, x = tooltip:AddLine()
-		tooltip:SetCell(y, 1, path.."store.tga", myProvider)
-		tooltip:SetCell(y, 2, _G.StoreMicroButton.tooltipText)
-		tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, _G.StoreMicroButton)
-	end
-
-	local y, x = tooltip:AddLine()
-	tooltip:SetCell(y, 1, path.."green.tga", myProvider)
-	tooltip:SetCell(y, 2, _G.MainMenuMicroButton.tooltipText)
-	tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, mainmenu)
-
-	tooltip:AddSeparator(10,0,0,0,0)
-
-	-- Adding Buttons of Main Game Menu
-	local GameMenuButtons = {
-		_G.GameMenuButtonSettings,
-		_G.GameMenuButtonEditMode,
-		_G.GameMenuButtonMacros,
-		_G.GameMenuButtonAddons,
-	}
-
-	for _, MenuButton in pairs(GameMenuButtons) do
-		if MenuButton then
-			local y, x = tooltip:AddLine()
-			tooltip:SetCell(y, 2, MenuButton:GetText())
-			tooltip:SetLineScript(y, "OnMouseUp", MouseHandler, function() MenuButton:Click() end)
-		end
-	end
-
-	tooltip:SetAutoHideDelay(0.01, self)
-	tooltip:SmartAnchorTo(self)
-	tooltip:Show()
 end
 
 function dataobj:SetDB(database)
@@ -314,12 +222,6 @@ local function OnUpdate(self, elapsed)
 	if counter >= delay then
 		dataobj:UpdateText()
 		counter = 0
-	end
-end
-
-local function OnDragStart(self, elapsed)
-	if tooltip then
-		LibQTip:Release(tooltip)
 	end
 end
 
